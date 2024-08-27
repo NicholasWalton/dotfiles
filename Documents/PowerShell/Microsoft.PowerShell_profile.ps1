0e54a36ef39632768f9296442c4b066528b7e15a
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-27 10:49:28 -0700

    oh-my-posh: Export and use powerline10k_rainbow

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,4 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Get-ChildItem "$PROFILE\..\Completions\" | ForEach-Object {
     . $_.FullName
 }
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "~/.mytheme.omp.json" | Invoke-Expression
