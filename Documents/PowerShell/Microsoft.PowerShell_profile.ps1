16573b0ddaa02995fe91bd7e11e0e26e7eac939b
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-27 10:34:56 -0700

    Add oh-my-posh to new PowerShell profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,4 @@ Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Get-ChildItem "$PROFILE\..\Completions\" | ForEach-Object {
     . $_.FullName
 }
+oh-my-posh init pwsh | Invoke-Expression
